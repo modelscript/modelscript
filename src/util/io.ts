4f1d354eec39ba8a41d38b6b1f858965341889bc
Mohamad Omar Nachawati <mnachawa@gmail.com>
2025-12-01 15:23:57 +0300

    add base64 encoding and compression routines

--- src/util/io.ts
+++ src/util/io.ts
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import pako from "pako";
+
 export interface Writer {
   write(string: string): void;
 }
@@ -14,4 +16,16 @@ export class StringWriter implements Writer {
   toString(): string {
     return this.#string;
   }
+}
+
+export function decodeAndInflateBase64Url(base64url: string): string {
+  const base64 = base64url.replaceAll("-", "+").replaceAll("_", "/");
+  const buffer = Buffer.from(base64, "base64");
+  return new TextDecoder().decode(pako.inflateRaw(buffer));
+}
+
+export function deflateAndEncodeBase64Url(text: string): string {
+  const buffer = pako.deflateRaw(Buffer.from(text, "utf8"));
+  const base64 = Buffer.from(buffer).toString("base64");
+  return base64.replaceAll("+", "-").replaceAll("/", "_");
 }
