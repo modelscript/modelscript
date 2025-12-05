79a30e5a9b560a927ebe9cf97fcdef05fd1798b6
Mohamad Omar Nachawati <mnachawa@gmail.com>
2025-12-05 18:42:28 +0300

    copy wasm binaries to build folder

--- packages/morsel/vite.config.ts
+++ packages/morsel/vite.config.ts
@@ -3,9 +3,27 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), nodePolyfills({ include: ["buffer"] })],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    nodePolyfills({ include: ["buffer"] }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./node_modules/web-tree-sitter/tree-sitter.wasm",
+          dest: "",
+        },
+        {
+          src: "../../node_modules/@modelscript/tree-sitter-modelica/tree-sitter-modelica.wasm",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   ssr: {
     noExternal: ["@primer/react", "monaco-editor"],
   },
