83994ce1827bfb9357e1ce662a79a8bd13193bd5
Mohamad Omar Nachawati <mnachawa@gmail.com>
2026-04-12 06:12:33 +0300

    refactor(core): merge ecad-canvas package into ecad

--- packages/ecad/src/index.ts
+++ packages/ecad/src/index.ts
@@ -2,3 +2,25 @@
 
 export * from "./gerber.js";
 export * from "./netlist.js";
+
+// Rendering engine
+export { DEFAULT_LAYERS, PCBRenderer } from "./engine/renderer.js";
+export type {
+  Camera,
+  Color,
+  DRCMarker,
+  LayerConfig,
+  Point2D,
+  RatsnestLine,
+  RenderPad,
+  RenderTrace,
+  RenderVia,
+} from "./engine/renderer.js";
+
+// Interactive router
+export { DEFAULT_ROUTING_RULES, PCBRouter } from "./engine/router.js";
+export type { RouterEvent, RouterEventHandler, RouterState, RoutingMode, RoutingRules } from "./engine/router.js";
+
+// Design rule checking
+export { DEFAULT_DESIGN_RULES, runDRC } from "./engine/drc.js";
+export type { DRCInput, DRCResult, DRCSeverity, DRCViolation, DesignRules } from "./engine/drc.js";
